7a3f9272bc772e17a576ca706907d454d2dc7eee
ethanlim <ethanlimck@gmail.com>
2025-05-22 11:35:36 +0000

    fixed routes part in py script

--- cgi/upload.py
+++ cgi/upload.py
@@ -67,60 +67,76 @@ elif request_method == "GET":
 
 	route_parts = route.rsplit("/")
 	#check on this ltr
-	file_name = route_parts[3]
-	#sys.stderr.write(f"Error: {file_name}\n")
+	if len(route_parts) > 3:
 
-	if os.path.exists(public_files_dir + file_name):
+		file_name = route_parts[3]
+		#sys.stderr.write(f"Error: {file_name}\n")
 
-		file_name_parts = file_name.split(".")
-		file_extension = file_name_parts[1]
+		if os.path.exists(public_files_dir + file_name):
 
-		with open(public_files_dir + file_name, "rb") as infile:
-			response_body = infile.read()
+			file_name_parts = file_name.split(".")
+			file_extension = file_name_parts[1]
+
+			with open(public_files_dir + file_name, "rb") as infile:
+				response_body = infile.read()
 
 
-		if(file_extension == "txt"):
-			content_type = "text/plain"
-			response_body = response_body.decode('utf-8')
-		elif file_extension == "html":
-			content_type = "text/html"
-			response_body = response_body.decode('utf-8')
-		elif file_extension == "css":
-			content_type = "text/css"
-			response_body = response_body.decode('utf-8')
-		elif file_extension == "js":
-			content_type = "application/javascript"
-			response_body = response_body.decode('utf-8')
-		elif file_extension == "json":
-			content_type = "application/json"
-			response_body = response_body.decode('utf-8')
-		elif file_extension == "jpg" or file_extension == "jpeg":
-			content_type = "image/jpeg"
-		elif file_extension == "png":
-			content_type = "image/png"
-		elif file_extension == "gif":
-			content_type = "image/gif"
+			if(file_extension == "txt"):
+				content_type = "text/plain"
+				response_body = response_body.decode('utf-8')
+			elif file_extension == "html":
+				content_type = "text/html"
+				response_body = response_body.decode('utf-8')
+			elif file_extension == "css":
+				content_type = "text/css"
+				response_body = response_body.decode('utf-8')
+			elif file_extension == "js":
+				content_type = "application/javascript"
+				response_body = response_body.decode('utf-8')
+			elif file_extension == "json":
+				content_type = "application/json"
+				response_body = response_body.decode('utf-8')
+			elif file_extension == "jpg" or file_extension == "jpeg":
+				content_type = "image/jpeg"
+			elif file_extension == "png":
+				content_type = "image/png"
+			elif file_extension == "gif":
+				content_type = "image/gif"
+			else:
+				content_type = "text/plain"
+			startline = "HTTP/1.1 200 OK\r\n"
+
 		else:
-			content_type = "text/plain"
-		
-		
-		print("HTTP/1.1 200 OK\r\n", end="")
-
-
+			startline = "HTTP/1.1 404 Not found\r\n"
+			response_body = "<!DOCTYPE html>"
+			response_body += "<html>"
+			response_body += "<head></head>"
+			response_body += "<body><center><h1> Error 404: Not found</h1></center></body>"
+			response_body += "</html>"
+			content_type = "text/html"
 	else:
-		print("HTTP/1.1 500 Internal Server Error\r\n", end="")
+		startline = "HTTP/1.1 403 Forbidden\r\n"
 		response_body = "<!DOCTYPE html>"
 		response_body += "<html>"
 		response_body += "<head></head>"
-		response_body += "<body><center><h1> Error 404: Not found</h1></center></body>"
+		response_body += "<body><center><h1> Error 403: Forbidden</h1></center></body>"
 		response_body += "</html>"
 		content_type = "text/html"
-	
+
+
+	print(startline, end = "")
 	print("Content-Type: " + content_type + "\r\n", end="")
 	print("Connection: keep-alive\r\n", end="")
 	print("Content-Length: " + str(len(response_body)) + "\r\n", end="")
 	print("\r\n", end="")
-	print(response_body)
+	sys.stdout.buffer.write(response_body)
+
+	# print(startline, end="", file=sys.stderr)
+	# print("Content-Type: " + content_type + "\r\n", end="", file=sys.stderr)
+	# print("Connection: keep-alive\r\n", end="", file=sys.stderr)
+	# print("Content-Length: " + str(len(response_body)) + "\r\n", end="", file=sys.stderr)
+	# print("\r\n", end="", file=sys.stderr)
+	# print(response_body, file=sys.stderr)
 
 elif request_method == "DELETE":
 	# print("HTTP/1.1 200 OK\r\n", end="")
