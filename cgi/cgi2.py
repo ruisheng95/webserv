b544b9f88cc9f97573d8ea7aba23dcfd4f34037f
ethanlim <ethanlimck@gmail.com>
2025-05-23 17:13:28 +0000

    added second cgi script for .php files

---
+++ cgi/cgi2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import cgi
+import cgitb
+
+startline = "HTTP/1.1 200 OK\r\n"
+response_body = "<!DOCTYPE html>"
+response_body += "<html>"
+response_body += "<head></head>"
+response_body += "<body><center><h1> Webpage from the cgi script for php files</h1></center></body>"
+response_body += "<center><img src = \"../cgi/public_files/furina.jpg\" alt = \"firefly picture\"><center>"
+response_body += "</html>"
+content_type = "text/html"
+
+print(startline, end = "")
+print("Content-Type: " + content_type + "\r\n", end="")
+print("Content-Length: " + str(len(response_body)) + "\r\n", end="")
+print("\r\n", end="")
+print(response_body)
